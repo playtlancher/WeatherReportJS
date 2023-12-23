38653edf991650d1a4fecbff8bf3d73561dc7d2d
Yoroslav Venediktov <playtlancher@gmail.com>
2023-12-23 17:03:50 +0200

    Add some improvements

--- LinkBuilder.js
+++ LinkBuilder.js
@@ -1,5 +1,4 @@
-export default function buildLink(city, days)
-{
+export default function buildLink(city, days) {
     let link = "http://api.weatherapi.com/v1/forecast.json?key=3dd7434243bb4e06933153229230509";
     link += "&q=" + city + "&days=" + days;
 
--- main.js
+++ main.js
@@ -2,14 +2,23 @@ import buildLink from "./LinkBuilder.js";
 import getWeather from "./GetWeather.js"
 import parseWeather from "./JsonParser.js";
 import configButtons from "./WeatherButtons.js";
-async function btnClick() {
-    document.getElementById("weather-table").style.display = "flex";
-    let link = buildLink(document.getElementById("city").value, 3);
-    getWeather(link).then((response)=>{
-        let weatherArray =  parseWeather(response);
-        configButtons(weatherArray);
 
+async function btnClick() {
+    let city = document.getElementById("city").value;
+    if (city === "") {
+        alert("Input city must contain something!")
+        return false;
+    }
+    let link = buildLink(city, 3);
+    getWeather(link).then((response) => {
+        if (response === undefined) {
+            alert("The city must exist");
+            return false;
+        }
+        let weatherArray = parseWeather(response);
+        configButtons(weatherArray);
     });
+    document.getElementById("weather-table").style.display = "flex";
 }
 
 document.getElementById("weatherForm").addEventListener("submit", btnClick);
